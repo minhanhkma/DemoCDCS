e7ca0abd03e92c209f16ebe1c3725f1d391547bc
minhanhkma <minhanhkma21@gmail.com>
2024-06-13 10:58:43 +0700

    Update ZAPService.java

--- src/main/java/example/demo/services/ZAPService.java
+++ src/main/java/example/demo/services/ZAPService.java
@@ -1,57 +1,55 @@
 package example.demo.services;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.zaproxy.clientapi.core.ClientApi;
 import org.zaproxy.clientapi.core.ClientApiException;
 import org.zaproxy.clientapi.core.ApiResponse;
 import org.zaproxy.clientapi.core.ApiResponseElement;
 
+
 @Service
 public class ZAPService {
-    private static final Logger logger = LoggerFactory.getLogger(ZAPService.class);
-
     private static final String ZAP_ADDRESS = "localhost";
     private static final int ZAP_PORT = 8080;
-    private static final String ZAP_API_KEY = "cegu8q7ehrmqg17l70pungpv6c";// API trên ZAP
+    private static final String ZAP_API_KEY = "cegu8q7ehrmqg17l70pungpv6c";
 
     private final ClientApi api = new ClientApi(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY);
 
-    public String scanURL(String url) {
-        try {
-            logger.info("Bắt đầu quét cho link: " + url);
+    public String scanURL(String url) throws ClientApiException {
+    
+        ApiResponse response = api.spider.scan(url, null, null, null, null);
+        String scanId = ((ApiResponseElement) response).getValue();
 
-            ApiResponse response = api.spider.scan(url, null, null, null, null);
-            String scanId = ((ApiResponseElement) response).getValue();
-            int progress;
-            do {
-                progress = Integer.parseInt(((ApiResponseElement) api.spider.status(scanId)).getValue());
-                try {
-                    Thread.sleep(1000); 
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } while (progress < 100);
+        int progress;
+        do {
+            progress = Integer.parseInt(((ApiResponseElement) api.spider.status(scanId)).getValue());
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (progress < 100);
 
-            logger.info("Hoàn thành quét cho link: " + url);
+        String title = "OWASP ZAP Report";
+        String template = "traditional-html";
+        String theme = "Mặc định";
+        String description = "Kết quả quét kiểm tra " + url;
+        String contexts = null;
+        String sites = null;
+        String sections = null;
+        String includedConfidences = null;
+        String includedRisks = null;
+        String reportFilename = null;
+        String reportFilenamePattern = null;
+        String reportDir = null;
+        String display = null;
 
-            String title = "OWASP ZAP Report";
-            String template = "traditional-html";
-            String theme = "default";
-            String description = "Kết quả quét kiểm tra" + url;
+        ApiResponse reportResponse = api.reports.generate(
+                title, template, theme, description, contexts, sites, sections,
+                includedConfidences, includedRisks, reportFilename, reportFilenamePattern, reportDir, display
+        );
 
-            ApiResponse reportResponse = api.reports.generate(
-                    title, template, theme, description, null, null, null,
-                    null, null, null, null, null, null
-            );
-
-            String report = ((ApiResponseElement) reportResponse).getValue();
-            logger.info("Báo cáo của link: " + url);
-            return report;
-        } catch (ClientApiException e) {
-            logger.error("Lỗi khi phân tích", e);
-            return "Lỗi khi phân tích";
-        }
+        String report = ((ApiResponseElement) reportResponse).getValue();
+        return report;
     }
 }
 
